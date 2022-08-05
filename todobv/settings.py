f1cf4b5ba186de26a46ce28c1e72441ad45417e5
Vasily <berkach@gmail.com>
2022-08-05 16:17:03 +0300

    lesson 2

--- todobv/settings.py
+++ todobv/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 
     # myapps
     'rest_framework',
+    'corsheaders',
     'todousers',
 ]
 
@@ -48,6 +52,7 @@ AUTH_USER_MODEL = 'todousers.Users'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
